ef35e6488dacb2e67b09d1fb7695b8a97be652f7
Juhaszky <juhasz.krisztian2000@gmail.com>
2024-11-06 21:24:39 +0100

    refact(search-api): Filtering stocks by name and symbols also sorting by matchScore 🔁

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -3,14 +3,23 @@ import { promises as fs } from 'fs';
 import { NextResponse } from "next/server";
 import path from 'path';
 
-export async function GET() {
-    const filePath = path.join(process.cwd(), 'app', 'data', 'mock.json'); // Absolute path to `mock.json`
-  
-  try {
-    const data = await fs.readFile(filePath, 'utf8'); // Read the file
-    const jsonData = JSON.parse(data); // Parse the JSON data
-    return NextResponse.json(jsonData); // Return the data as JSON
-  } catch (error) {
-    return NextResponse.json({ error: 'Failed to load data' + error }, { status: 500 });
-  }
+export async function GET(req: Request) {
+    const url = new URL(req.url);
+    const query = url.searchParams.get('query')?.toLowerCase() || '';
+    const filePath = path.join(process.cwd(), 'app', 'data', 'mock.json');
+
+    try {
+        const data = await fs.readFile(filePath, 'utf8');
+        const jsonData = JSON.parse(data);
+        const filteredResults = jsonData.bestMatches.filter((match: { name: string; symbol: string }) => {
+            return match.name.toLowerCase().includes(query) || match.symbol.toLowerCase().includes(query);
+        });
+
+        const sortedResults = filteredResults.sort((a: { matchScore: string }, b: { matchScore: string }) => {
+            return parseFloat(b.matchScore) - parseFloat(a.matchScore);
+        });
+        return NextResponse.json({ bestMatches: sortedResults });
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to load data' + error }, { status: 500 });
+    }
 }
