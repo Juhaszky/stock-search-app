6a982f38065f1f7dcbaea328eccbccc97c0b9126
Juhaszky <juhasz.krisztian2000@gmail.com>
2024-11-07 00:08:13 +0100

    refact(searchLayout): Types added to api calls 🔁

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import Search from "./components/Search";
 
+import { Match } from "./models/Match";
+import { SearchResponse } from "./models/SearchResponse";
+
 const SearchLayout = async (props: {
     searchParams?: Promise<{
         query?: string;
@@ -9,13 +12,13 @@ const SearchLayout = async (props: {
 }) => {
     const searchParams = await props.searchParams;
     const query = searchParams?.query || '';
-    let searchResults: { symbol: string, name: string, matchScore: number }[] = [];
+    let searchResults: Match[] = [];
     if (query) {
         try {
-            const fetchedData = await fetch(`http://localhost:3000/api/search?query=${query}`);
-            const data = await fetchedData.json();
-            searchResults = data.bestMatches || [];
-            searchResults.sort((a, b) => b.matchScore - a.matchScore)
+            const fetchedData: SearchResponse = await fetch(`http://localhost:3000/api/search?query=${query}`)
+            .then(res => res.json());
+            searchResults = fetchedData.bestMatches || [];
+            searchResults.sort((a, b) => b['9. matchScore'] - a['9. matchScore'])
         } catch (err) {
             console.error("Error fetching search results:", err);
         }
@@ -24,23 +27,23 @@ const SearchLayout = async (props: {
         <div className="flex flex-col items-center gap-4 w-full h-full p-4">
             <h1 className="font-bold text-3xl">Stock-Search-App</h1>
             <div className="flex justify-center relative w-full md:w-1/2">
-                <Search placeholder="Search..."></Search>
+                <Search placeholder="Enter symbol or name..."></Search>
             </div>
             {query && (
-                <div className="max-w-md -mt-4 w-full md:w-1/2 text-gray-900 border border-zinc-500 rounded-lg bg-zinc-600">
-                    <ul>
+                <div className="max-w-md -mt-4 w-full md:w-1/2 text-gray-900 border border-zinc-500 rounded-lg bg-zinc-600 overflow-y-scroll">
+                    <ul >
                         {searchResults.length > 0 ? (
                             searchResults.map((result, index) => (
 
-                                <li key={index} className="p-2 border-b border-zinc-600 bg-zinc-600 hover:bg-gray-700 hover:cursor-pointer rounded-lg">
-                                    <Link href={`/detail/${result.symbol}`}>
+                                <li key={index} className="p-2 border-b border-zinc-700 bg-zinc-600 hover:bg-gray-700 hover:cursor-pointer rounded-lg">
+                                    <Link href={`/detail/${result['1. symbol']}`}>
                                         <p>
-                                            <span className="font-bold">Symbol:</span> {result.symbol}
+                                            <span className="font-bold">Symbol:</span> {result['1. symbol']}
                                         </p>
                                         <p>
-                                            <span className="font-bold">Name: </span>{result.name}
+                                            <span className="font-bold">Name: </span>{result['2. name']}
                                         </p>
-                                        <p><span className="font-bold">MatchScore:</span> {result.matchScore}</p>
+                                        <p><span className="font-bold">MatchScore:</span> {result['9. matchScore']}</p>
 
                                         <div></div>
                                     </Link>
