8bd667a3a3f69c4cc324cd1c49cda9dda1148d9f
Juhaszky <juhasz.krisztian2000@gmail.com>
2024-11-07 17:30:51 +0100

    styling(Favorites): Background color and round added 🧑‍🎨

--- app/favorites/components/Favorites.tsx
+++ app/favorites/components/Favorites.tsx
@@ -9,7 +9,7 @@ const Favorites = ({ favorites }: { favorites: StockSymbolData[] }) => {
                 <p>You have no favorite stocks saved yet.</p>
             ) : (
                 <div className="w-full md:w-1/2 text-gray-900">
-                    <ul>
+                    <ul className="bg-zinc-600 rounded-lg">
                         {favorites.map((stock, index) => (
                             <Link key={index} href={`/detail/${stock.stockSymbol}`}>
                                 <li key={index} className="p-6 border-b border-zinc-700 bg-zinc-600 hover:bg-gray-700 hover:cursor-pointer rounded-lg">
